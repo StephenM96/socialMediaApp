21fce401db049ccd33d2d9f7b1572bbdf2972545
unknown <mossysm96@gmail.com>
2023-05-31 21:10:05 -0500

    need to delete this and type from scratch

--- data/postsData.js
+++ data/postsData.js
@@ -11,30 +11,6 @@ let posts = [
     content: "Content for post 2 is here",
     imgUrl: "https://placehold.co/300x200"
   },
-  {
-    id: "3",
-    title: "Title of Post 3",
-    content: "Content for post 3 is here",
-    imgUrl: "https://placehold.co/300x200"
-  },
-  {
-    id: "4",
-    title: "Title of Post 4",
-    content: "Content for post 4 is here",
-    imgUrl: "https://placehold.co/300x200"
-  },
-  {
-    id: "5",
-    title: "Title of Post 5",
-    content: "Content for post 5 is here",
-    imgUrl: "https://placehold.co/300x200"
-  },
-  {
-    id: "6",
-    title: "Title of Post 6",
-    content: "Content for post 6 is here",
-    imgUrl: "https://placehold.co/300x200"
-  },
 
 ]
 
